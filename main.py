1254d69ede7ec20c6db11a7d91d851aaf3e30f05
Matvii <matvijhlan@gmail.com>
2026-09-12 16:17:52 +0300

    Update main 2

--- main.py
+++ main.py
@@ -30,13 +30,14 @@ load_dotenv(dotenv_path=evn_path)
 DATABASE_URL = os.getenv("DATABASE_URL")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# Встав сюди ID людей, яких треба забанити
 BANNED_USERS = os.getenv("BANNED_USERS")  # замініть на потрібний ID
 
 class BlockMiddleware(BaseMiddleware):
-    async def call(self, handler, event: TelegramObject, data):
+    async def __call__(self, handler, event: TelegramObject, data):
         user = data.get("event_from_user")
         if user and user.id in BANNED_USERS:
-            return # Бот просто мовчить і нічого не робить
+            return  # Мовчки ігноруємо
         return await handler(event, data)
 
 bot = Bot(token=BOT_TOKEN)
@@ -732,13 +733,13 @@ async def get_db_pool():
 async def main():
     db_pool = await get_db_pool()
     dp.workflow_data["db_pool"] = db_pool
-    
+
     dp.message.outer_middleware(BlockMiddleware())
     dp.callback_query.outer_middleware(BlockMiddleware())
 
     asyncio.create_task(start_dummy_server())
     await dp.start_polling(bot)
-
+    
 if __name__ == "__main__":
   try:
     print("Бот запускається...")
